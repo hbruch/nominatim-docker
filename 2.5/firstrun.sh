5ad58e62314c774acf7d3d2c0fe53a7347164c3f
Thomas Nordquist <thomas@nordquist.de>
2017-05-29 14:59:04 +0200

    Refactor replication URL to environmental

--- 2.5/firstrun.sh
+++ 2.5/firstrun.sh
@@ -1,18 +1,29 @@
 # Load initial data
-echo Running on $THREADS cores
+echo Running on $THREADS cores, you can changes this by setting the THREADS environmental
 
-if [ "$PBF_FILE" = "" ]; then
-  echo "You need to specify the environmental PBF_FILE"
-  echo "docker run -e http://download.geofabrik.de/europe/monaco-latest.osm.pbf ..."
+if [ "$PBF_URL" = "" ]; then
+  echo "You need to specify the environmental PBF_URL"
+  echo "docker run -e PBF_URL=http://download.geofabrik.de/europe/monaco-latest.osm.pbf ..."
+  exit 1
 fi;
+
+if [ "$REPLICATION_URL" = "" ]; then
+  echo "You need to specify the environmental REPLICATION_URL"
+  echo "docker run -e REPLICATION_URL=http://download.geofabrik.de/europe/germany-updates/ ..."
+  exit 1
+else
+  sed -i "s|__REPLICATION_URL__|$REPLICATION_URL|g" /var/www/settings/local.php
+fi;
+
+
 
 # Update postgres config to improve import performance
 sed -i "s/fsync = on/fsync = off/g" /etc/postgresql/9.5/main/postgresql.conf
 sed -i "s/full_page_writes = on/full_page_writes = off/g" /etc/postgresql/9.5/main/postgresql.conf
 
-curl -L "$PBF_FILE" --create-dirs -o /app/src/data.osm.pbf
-curl -L -o /app/src/data/country_osm_grid.sql.gz http://www.nominatim.org/data/country_grid.sql.gz
-service postgresql start && \
+curl -L "$PBF_URL" --create-dirs -o /app/src/data.osm.pbf && \
+    curl -L -o /app/src/data/country_osm_grid.sql.gz http://www.nominatim.org/data/country_grid.sql.gz && \
+    service postgresql start && \
     sudo -u postgres psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='nominatim'" | grep -q 1 || sudo -u postgres createuser -s nominatim && \
     sudo -u postgres psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='www-data'" | grep -q 1 || sudo -u postgres createuser -SDR www-data && \
     sudo -u  postgres psql postgres -c "DROP DATABASE IF EXISTS nominatim" && \
