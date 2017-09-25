e3cbd6a7f2606fbbcd433dab8e5b065218fbd779
Thomas Nordquist <thomas@nordquist.de>
2017-09-25 19:06:26 +0200

    Migrate to nominatim 3.0.1

--- latest/firstrun.sh
+++ latest/firstrun.sh
@@ -12,28 +12,36 @@ if [ "$REPLICATION_URL" = "" ]; then
   echo "docker run -e REPLICATION_URL=http://download.geofabrik.de/europe/germany-updates/ ..."
   exit 1
 else
-  sed -i "s|__REPLICATION_URL__|$REPLICATION_URL|g" /var/www/settings/local.php
+  sed -i "s|__REPLICATION_URL__|$REPLICATION_URL|g" $USERHOME/Nominatim/settings/local.php
 fi;
 
 
 
 # Update postgres config to improve import performance
-sed -i "s/fsync = on/fsync = off/g" /etc/postgresql/9.3/main/postgresql.conf
-sed -i "s/full_page_writes = on/full_page_writes = off/g" /etc/postgresql/9.3/main/postgresql.conf
+sed -i "s/fsync = on/fsync = off/g" /etc/postgresql/9.5/main/postgresql.conf
+sed -i "s/full_page_writes = on/full_page_writes = off/g" /etc/postgresql/9.5/main/postgresql.conf
 
 echo Downloading map data from "$PBF_URL"
-curl -L "$PBF_URL" --create-dirs -o /app/src/data.osm.pbf && \
-    curl -L -o /app/src/data/country_osm_grid.sql.gz http://www.nominatim.org/data/country_grid.sql.gz && \
+curl -L "$PBF_URL" --create-dirs -o $USERHOME/data.osm.pbf && \
+    #curl -L -o /app/src/data/country_osm_grid.sql.gz http://www.nominatim.org/data/country_grid.sql.gz && \
     service postgresql start && \
+    sudo -u postgres createuser -s $USERNAME && \
+    sudo -u postgres createuser www-data && \
     sudo -u postgres psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='nominatim'" | grep -q 1 || sudo -u postgres createuser -s nominatim && \
     sudo -u postgres psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='www-data'" | grep -q 1 || sudo -u postgres createuser -SDR www-data && \
     sudo -u  postgres psql postgres -c "DROP DATABASE IF EXISTS nominatim" && \
-    useradd -m -p password1234 nominatim && \
-    chown -R nominatim:nominatim ./src && \
-    sed -i "s/number-processes 1/number-processes $THREADS/g" src/build/utils/setup.php && \
-    sudo -u nominatim ./src/build/utils/setup.php --osm-file /app/src/data.osm.pbf --all --threads $THREADS && \
+    chown -R nominatim:nominatim ./Nominatim && \
+    cp Nominatim/settings/local.php Nominatim/build/settings/ && \
+    cd Nominatim/build && \
+    sudo -u nominatim utils/setup.php --osm-file $USERHOME/data.osm.pbf --all --threads $THREADS && \
+    sudo -u nominatim utils/update.php --init-updates &&
     service postgresql stop && \
     touch /app/installed
 
-sed -i "s/fsync = off/fsync = on/g" /etc/postgresql/9.3/main/postgresql.conf
-sed -i "s/full_page_writes = off/full_page_writes = on/g" /etc/postgresql/9.3/main/postgresql.conf
+
+sed -i "s/fsync = off/fsync = on/g" /etc/postgresql/9.5/main/postgresql.conf
+sed -i "s/full_page_writes = off/full_page_writes = on/g" /etc/postgresql/9.5/main/postgresql.conf
+
+
+    #sed -i "s/number-processes 1/number-processes $THREADS/g" src/build/utils/setup.php && \
+    #useradd -m -p password1234 nominatim && \
--- latest/local.php
+++ latest/local.php
@@ -1,10 +1,12 @@
 <?php
  // Paths
- @define('CONST_Postgresql_Version', '9.3');
- @define('CONST_Postgis_Version', '2.1');
+ //@define('CONST_Postgresql_Version', '9.5');
+ //@define('CONST_Postgis_Version', '2.1');
  // Website settings
- @define('CONST_Website_BaseURL', '/');
+ @define('CONST_Website_BaseURL', '/nominatim/');
  @define('CONST_Replication_Url', '__REPLICATION_URL__');
  @define('CONST_Replication_MaxInterval', '86400');     // Process each update separately, osmosis cannot merge multiple updates
  @define('CONST_Replication_Update_Interval', '86400');  // How often upstream publishes diffs
  @define('CONST_Replication_Recheck_Interval', '900');   // How long to sleep if no update found yet
+ @define('CONST_Pyosmium_Binary', '/usr/local/bin/pyosmium-get-changes');
+ @define('CONST_Osm2pgsql_Flatnode_File', '/srv/nominatim/flatnode.file');
